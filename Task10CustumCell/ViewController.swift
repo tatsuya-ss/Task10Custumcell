7d86fa1f97f0e4152da907269d76b98b9755d5a6
tatsuya-ss <72326299+tatsuya-ss@users.noreply.github.com>
2020-10-19 05:32:23 +0900

    Task10のcustumcell作成

---
+++ Task10CustumCell/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Task10CustumCell
+//
+//  Created by 坂本龍哉 on 2020/10/19.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Task10CustumCell/prefectureTableViewController.swift
@@ -0,0 +1,51 @@
+//
+//  prefectureTableViewController.swift
+//  Task10CustumCell
+//
+//  Created by 坂本龍哉 on 2020/10/19.
+//
+
+import UIKit
+
+class prefectureTableViewController: UITableViewController {
+
+    private let prefectureArray:Array = ["北海道","青森県","岩手県","宮城県","秋田県","山形県","福島県","茨城県","栃木県","群馬県","埼玉県","千葉県","東京都","神奈川県","新潟県","富山県","石川県","福井県","山梨県","長野県","岐阜県","静岡県","愛知県","三重県","滋賀県","京都府","大阪府","兵庫県","奈良県","和歌山県","鳥取県","島根県","岡山県","広島県","山口県","徳島県","香川県","愛媛県","高知県","福岡県","佐賀県","長崎県","熊本県","大分県","宮崎県","鹿児島県","沖縄県"]
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return prefectureArray.count
+    }
+
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        var identifier = ""
+        if indexPath.row % 3 == 0 {
+            identifier = "Cell1"
+        }else if indexPath.row % 3 == 1{
+            identifier = "Cell2"
+        }else{
+            identifier = "Cell3"
+        }
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: identifier, for: indexPath)
+
+        var preflabel = cell.viewWithTag(1) as! UILabel
+        preflabel.text = prefectureArray[indexPath.row]
+        var countlabel = cell.viewWithTag(2) as! UILabel
+        countlabel.text = "\(indexPath.row + 1)番目の都道府県です"
+        
+        return cell
+    }
+
+}
